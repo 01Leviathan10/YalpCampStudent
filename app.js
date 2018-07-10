867a5e6e5f1ae3af5c649740644f91c154c37adc
01Leviathan10 <marko.milicevic0092@gmail.com>
2018-07-10 11:29:52 +0200

    Adding DB to YalpCamp

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+var express     = require("express"),
+    bodyParser  = require("body-parser"),
+    app         = express(),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yepl_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+//schema
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create( 
+//     {
+//     name: "Borsko Jezero", 
+//     image: "http://www.camping.rs/wp-content/uploads/2013/08/111-710x473.jpg"
+//     }, function (err, campground) {
+//             if (err) {
+//                 console.log(err);
+//             }else {
+//                 console.log("NEWLY CREATED CAMPGROUND: ");
+//                 console.log(campground);
+//                 }
+// });
 
 var campgrounds = [
     {name: "Dunav", image: "http://www.camping.rs/wp-content/uploads/2013/08/2809-044.jpg"},
@@ -16,7 +40,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds:campgrounds});
+    //all camprounds from db
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    })
 });
 
 app.get("/campgrounds/new", function (req, res) {
@@ -27,8 +58,14 @@ app.post("/campgrounds", function (req,res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampgrund = {name: name, image: image};
-    campgrounds.push(newCampgrund);
-    res.redirect("/campgrounds");
+    //create new campground andsav to DB
+    Campground.create(newCampgrund, function (err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 
